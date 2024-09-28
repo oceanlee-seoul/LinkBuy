e7fc5901fc621b90185f9e27d96b9f37ff05b2ee
Daeyang lee <eodid007@gmail.com>
2024-09-29 01:43:26 +0900

    fix: font load link 태그로 변경

--- src/styles/fonts.css.ts
+++
@@ -1,9 +0,0 @@
-import { globalFontFace } from '@vanilla-extract/css';
-
-const notoSans = 'Noto Sans KR';
-
-globalFontFace(notoSans, {
-  src: 'url(https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap)',
-});
-
-export { notoSans };
--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -1,9 +1,8 @@
 import { globalStyle } from '@vanilla-extract/css';
-import { notoSans } from './fonts.css.ts';
 
 import './layers.css.ts';
 import './reset.css.ts';
 
 globalStyle('body', {
-  fontFamily: notoSans,
+  fontFamily: 'Noto Sans KR',
 });
